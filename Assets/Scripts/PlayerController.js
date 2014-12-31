77b4a88d60955f8a942a68a09d1d2e0adae4ef19
Tr1ckX <attila.mirk@gmail.com>
2014-12-31 19:14:57 +0000

    moving the ball JS script

---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var speed : float;
+
+function FixedUpdate ()
+{
+	var moveHorizontal : float= Input.GetAxis("Horizontal");
+	var moveVertical : float = Input.GetAxis("Vertical");
+	
+	var movement : Vector3 = new Vector3(moveHorizontal, 0.0, moveVertical);
+	
+	rigidbody.AddForce (movement * speed * Time.deltaTime);
+
+}
